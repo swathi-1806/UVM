a4ebcb3a9826eab2b897cbf618ea65ad551f5f5b
swathi bawge <swathiveda4632@gmail.com>
2026-04-14 21:05:37 +0530

    Update mem_tx.sv

--- phase_2_of_uvm/mem_tx.sv
+++ phase_2_of_uvm/mem_tx.sv
@@ -1,13 +1,25 @@
 class mem_tx extends uvm_sequence_item;
 
-//factory registration
-`uvm_object_utils(mem_tx)
+//properties
+rand bit wr_rd;
+rand bit [31:0]wdata;
+	 bit [31:0]rdata;
+rand bit [31:0]addr;
 
-//consructor
+
+//factory registration + feild registration
+`uvm_object_utils_begin(mem_tx)
+	`uvm_field_int(wr_rd, UVM_ALL_ON | UVM_DEC)
+	`uvm_field_int(wdata, UVM_ALL_ON |UVM_DEC)
+	`uvm_field_int(rdata, UVM_ALL_ON |UVM_DEC)
+	`uvm_field_int(addr, UVM_ALL_ON |UVM_HEX)
+`uvm_object_utils_end
+
 //constructors
 function new(string name="mem_tx");
 	super.new(name);
 endfunction
 
 endclass
+
 
