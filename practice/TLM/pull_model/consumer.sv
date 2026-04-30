45ecd3991dd8048e497605c18dbc64dc24fc6900
swathi bawge <swathiveda4632@gmail.com>
2026-04-30 21:44:55 +0530

    Update consumer.sv

--- practice/TLM/pull_model/consumer.sv
+++ practice/TLM/pull_model/consumer.sv
@@ -4,19 +4,17 @@ class consumer extends uvm_component;
 
 //instantiate tlm1.0 base class
  uvm_blocking_get_port#(my_tx)  consumer_port_h;
-my_tx tx;
+
 `NEW_COMP
   
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 	consumer_port_h=new("consumer_port_h",this);
-  
-    // CREATE the object here
-  tx = my_tx::type_id::create("tx");
    endfunction 
 
 //call the get method to fetch the tx from producer
 task run_phase(uvm_phase phase);
+	my_tx tx;
   repeat(2) begin
     `uvm_info("consumer","calling the get method",UVM_NONE)
 
