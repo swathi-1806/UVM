c92f3c8beca65a458f698b2ac33d8958af1b9e77
swathi bawge <swathiveda4632@gmail.com>
2026-04-30 19:48:29 +0530

    Update output.sv

--- practice/TLM/pull_model/output.sv
+++ practice/TLM/pull_model/output.sv
@@ -8,64 +8,67 @@
 # uvm_test_top           tlm_test               -     @355 
 #   env_h                env                    -     @367 
 #     consumer_h         consumer               -     @383 
-#       consumer_imp_h   uvm_blocking_put_imp   -     @391 
+#       consumer_port_h  uvm_blocking_get_port  -     @391 
 #     producer_h         producer               -     @375 
-#       producer_port_h  uvm_blocking_put_port  -     @400 
+#       producer_imp_h   uvm_blocking_get_imp   -     @401 
 # ---------------------------------------------------------
 # 
-# UVM_INFO producer.sv(28) @ 10: uvm_test_top.env_h.producer_h [producer] producer put called @ 10
+# UVM_INFO consumer.sv(21) @ 0: uvm_test_top.env_h.consumer_h [consumer] calling the get method
+# UVM_INFO producer.sv(20) @ 0: uvm_test_top.env_h.producer_h [producer] get method called
+# UVM_INFO producer.sv(27) @ 0: uvm_test_top.env_h.producer_h [producer] producer randomized tx is @ 0
 # ------------------------------
 # Name     Type      Size  Value
 # ------------------------------
-# tx       my_tx     -     @420 
+# tx_t     my_tx     -     @426 
 #   wr_rd  integral  1     'h1  
-#   addr   integral  8     'h5a 
-#   data   integral  8     'h3a 
+#   addr   integral  8     'h9c 
+#   data   integral  8     'h6a 
 # ------------------------------
-# UVM_INFO consumer.sv(19) @ 25: uvm_test_top.env_h.consumer_h [consumer] consumer consumed data @ 25
+# UVM_INFO consumer.sv(25) @ 0: uvm_test_top.env_h.consumer_h [consumer] received tx is @ 0
 # ------------------------------
 # Name     Type      Size  Value
 # ------------------------------
-# tx       my_tx     -     @420 
+# tx_t     my_tx     -     @426 
 #   wr_rd  integral  1     'h1  
-#   addr   integral  8     'h5a 
-#   data   integral  8     'h3a 
+#   addr   integral  8     'h9c 
+#   data   integral  8     'h6a 
 # ------------------------------
-# UVM_INFO producer.sv(28) @ 35: uvm_test_top.env_h.producer_h [producer] producer put called @ 35
+# UVM_INFO consumer.sv(21) @ 0: uvm_test_top.env_h.consumer_h [consumer] calling the get method
+# UVM_INFO producer.sv(20) @ 0: uvm_test_top.env_h.producer_h [producer] get method called
+# UVM_INFO producer.sv(27) @ 0: uvm_test_top.env_h.producer_h [producer] producer randomized tx is @ 0
 # ------------------------------
 # Name     Type      Size  Value
 # ------------------------------
-# tx       my_tx     -     @438 
+# tx_t     my_tx     -     @435 
 #   wr_rd  integral  1     'h1  
-#   addr   integral  8     'h89 
-#   data   integral  8     'h75 
+#   addr   integral  8     'hbc 
+#   data   integral  8     'h61 
 # ------------------------------
-# UVM_INFO consumer.sv(19) @ 50: uvm_test_top.env_h.consumer_h [consumer] consumer consumed data @ 50
+# UVM_INFO consumer.sv(25) @ 0: uvm_test_top.env_h.consumer_h [consumer] received tx is @ 0
 # ------------------------------
 # Name     Type      Size  Value
 # ------------------------------
-# tx       my_tx     -     @438 
+# tx_t     my_tx     -     @435 
 #   wr_rd  integral  1     'h1  
-#   addr   integral  8     'h89 
-#   data   integral  8     'h75 
+#   addr   integral  8     'hbc 
+#   data   integral  8     'h61 
 # ------------------------------
-# UVM_INFO /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_objection.svh(1270) @ 60: reporter [TEST_DONE] 'run' phase is ready to proceed to the 'extract' phase
-# UVM_INFO /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_report_server.svh(847) @ 60: reporter [UVM/REPORT/SERVER] 
+# UVM_INFO /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_report_server.svh(847) @ 0: reporter [UVM/REPORT/SERVER] 
 # --- UVM Report Summary ---
 # 
 # ** Report counts by severity
-# UVM_INFO :   10
+# UVM_INFO :   13
 # UVM_WARNING :    0
 # UVM_ERROR :    0
 # UVM_FATAL :    0
 # ** Report counts by id
 # [Questa UVM]     2
 # [RNTST]     1
-# [TEST_DONE]     1
 # [UVM/RELNOTES]     1
 # [UVMTOP]     1
-# [consumer]     2
-# [producer]     2
+# [consumer]     4
+# [producer]     4
 # 
 # ** Note: $finish    : /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_root.svh(517)
-#    Time: 60 ns  Iteration: 69  Instance: /top
+#    Time: 0 ns  Iteration: 269  Instance: /top
+
