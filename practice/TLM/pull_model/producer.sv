de7b27ce6d5f018e053d89fc5762dfd731ac4dcd
swathi bawge <swathiveda4632@gmail.com>
2026-04-30 19:45:52 +0530

    Update producer.sv

--- practice/TLM/pull_model/producer.sv
+++ practice/TLM/pull_model/producer.sv
@@ -4,7 +4,7 @@ class producer extends uvm_component;
 
 //instantiate tlm1.0 base class
  uvm_blocking_get_imp#(my_tx,producer)  producer_imp_h;
-
+my_tx tx;
 
 `NEW_COMP
   
@@ -14,13 +14,18 @@ class producer extends uvm_component;
 	tx = my_tx::type_id::create("tx");
  endfunction
 
-//provide the implen=metation of get method
-task get(output my_tx tx);
-`uvm_info("producer","get method called",UVM_NONE)
-	tx.randomize();
-	`uvm_info("producer",$sformatf("producer randomized tx is %s",tx.print()),UVM_NONE)
-	tx_t = tx;//assign the randomize tx to task output argument
-	//$cast(tx_t,tx);
-endtask
+// Implementation of get method
+task get(output my_tx tx_t);
 
+  `uvm_info("producer","get method called",UVM_NONE)
+
+  // Create fresh object every call
+  tx_t = my_tx::type_id::create("tx_t");
+
+  tx_t.randomize();
+
+  `uvm_info("producer",$sformatf("producer randomized tx is @ %0t",$time),UVM_NONE)
+  tx_t.print();
+
+endtask
 endclass
