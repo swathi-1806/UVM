b879ffb2f52cb62426113127a76a15d053945e7c
swathi bawge <swathiveda4632@gmail.com>
2026-05-15 22:53:18 +0530

    Add files via upload

---
+++ resource_db/set_nd_read_by_type/output.sv
@@ -0,0 +1,28 @@
+# UVM_INFO /usr/share/questa/questasim/verilog_src/questa_uvm_pkg-1.2/src/questa_uvm_pkg.sv(277) @ 0: reporter [Questa UVM] QUESTA_UVM-1.2.3
+# UVM_INFO /usr/share/questa/questasim/verilog_src/questa_uvm_pkg-1.2/src/questa_uvm_pkg.sv(278) @ 0: reporter [Questa UVM]  questa_uvm::init(+struct)
+# UVM_INFO @ 0: reporter [RNTST] Running test mem_base_test...
+# ----------------------------------------------------
+# Name             Type                    Size  Value
+# ----------------------------------------------------
+# drv_h            mem_drv                 -     @389 
+#   rsp_port       uvm_analysis_port       -     @406 
+#   seq_item_port  uvm_seq_item_pull_port  -     @397 
+#   name           string                  4     VLSI 
+# ----------------------------------------------------
+# UVM_INFO /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_objection.svh(1270) @ 50: reporter [TEST_DONE] 'run' phase is ready to proceed to the 'extract' phase
+# UVM_INFO /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_report_server.svh(847) @ 50: reporter [UVM/REPORT/SERVER] 
+# --- UVM Report Summary ---
+# 
+# ** Report counts by severity
+# UVM_INFO :    5
+# UVM_WARNING :    0
+# UVM_ERROR :    0
+# UVM_FATAL :    0
+# ** Report counts by id
+# [Questa UVM]     2
+# [RNTST]     1
+# [TEST_DONE]     1
+# [UVM/RELNOTES]     1
+# 
+# ** Note: $finish    : /usr/share/questa/questasim/verilog_src/uvm-1.2/src/base/uvm_root.svh(517)
+#    Time: 50 ns  Iteration: 68  Instance: /tb
